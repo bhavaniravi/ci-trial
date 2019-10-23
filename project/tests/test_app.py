e2c5e04e39b29748133e59fca8dc7f9ba57fa9af
Bhavani Ravi <bhava0895@gmail.com>
2019-10-23 13:50:53 +0530

    add sample test cases

---
+++ project/tests/test_app.py
@@ -0,0 +1,18 @@
+from ..app import Calculator
+import pytest
+
+def test_calculator():
+    assert type(Calculator()) is Calculator
+
+def test_add():
+    calc = Calculator()
+    assert calc.add(2, 3) == 2 + 3
+
+def test_subtract():
+    calc = Calculator()
+    assert calc.sub(2, 3) == 2 - 3
+
+def test_fails_on_string():
+    calc = Calculator()
+    with pytest.raises(TypeError):
+        calc.sub("2", "3")
